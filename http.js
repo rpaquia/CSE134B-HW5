9d3f5778b11fbd0b394b8d02eea97aeaefbc9e75
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-10 15:25:01 -0800

    fixes

---
+++ http.js
@@ -0,0 +1,7 @@
+// GET method
+function getFunc() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://httpbin.org/post", true);
+    xhr.onload = function() {handleResponse(xhr);};
+    xhr.send(null); 
+}
