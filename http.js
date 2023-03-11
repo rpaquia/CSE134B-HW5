a5451c6cce76a6529f30811252971d80d027a303
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-10 16:05:01 -0800

    Fleshing out basic structure

--- http.js
+++ http.js
@@ -1,11 +1,17 @@
-
+// Variables for inputs
 const artNameInp = document.getElementById('articleName');
+const textAreaInp = document.getElementById('article_body');
+
+// Variables for buttons
 const 
+const
+const
+const
 
 // GET method
 function getFunc() {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://httpbin.org/post", true);
+    xhr.open("GET", "https://httpbin.org/get", true);
     xhr.onload = function() {handleResponse(xhr);};
     xhr.send(null); 
 }
