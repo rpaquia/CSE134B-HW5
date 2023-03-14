500f9b551ae2b53a8fd8352eddef8c00ba668f6a
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-13 17:25:59 -0700

    PUT Method

--- http.js
+++ http.js
@@ -1,17 +1,2 @@
-// Variables for inputs
-const artNameInp = document.getElementById('articleName');
-const textAreaInp = document.getElementById('article_body');
-
-// Variables for buttons
-const getData = document.getElementById(getting);
-const postData = document.getElementById(posting);
-const putData = document.getElementById(putting);
-const deleteData = document.getElementById(deleting);
-
-// GET method
-function getFunc() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://httpbin.org/get", true);
-    xhr.onload = function() {handleResponse(xhr);};
-    xhr.send(null); 
-}
+// PUT Method
+async
