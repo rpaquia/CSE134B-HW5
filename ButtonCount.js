0f67528f804f0a89b769fd2deda0d69788a608af
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-15 01:48:36 -0700

    Update ButtonCount.js

--- ButtonCount.js
+++ ButtonCount.js
@@ -5,27 +5,22 @@ class ButtonCount extends HTMLElement {
         // Attach Shadow DOM to web component to protect its scope
         const shadow = this.attachShadow({mode:'open'});
         
-        // Create button
-        const button = document.createElement('button');
-        // Set button's attributes
-        button.innerHTML = 'Times Clicker: 0'
-        button.setAttribute('type', 'button'); // type='button'
-        button.setAttribute('id', 'counter'); // id='counter
-        button.setAttribute('onclick', 'Increment()') // onclick='Increment()'
-        document.querySelector('body').appendChild(button);
-
-        // Set up counter
-        function Increment(){
-            let count = 0;
-            let btn = document.getElementById('counter');
-            
-            btn.onclick = function () {
-                count++;
-                button.innerHTML = 'Times Clicker' + count;
-            }
-        }
+        shadow.innerHTML = `
+            <button type='button' id='counter' onclick='Increment()'
+            <script>
+                function Increment() {
+                    let count = 0;
+                    let btn = document.getElementById('counter');
+                    
+                    btn.onclick = function () {
+                        count++;
+                        button.innerHTML = 'Times Clicker' + count;
+                    }
+                }
+            </script>
         
-
+        `
+        
     }
 }
 // Define ButtonCLicker class in custom elements registry so I may use it
