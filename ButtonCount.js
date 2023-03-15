af8dd2c136c5da743c799b95c7fc8a8f772dc269
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-15 05:48:07 -0700

    Update ButtonCount.js

--- ButtonCount.js
+++ ButtonCount.js
@@ -1,11 +1,11 @@
 // Set up counter
 function Increment() {
-    
+    let count = 0;
     let btn = document.getElementById('counter');
             
     btn.onclick = function () {
-        let count = 0;
-        count++;
+        
+        count = count + 1;
         btn.innerHTML = 'Times Clicker: ' + count;
         }
     }
