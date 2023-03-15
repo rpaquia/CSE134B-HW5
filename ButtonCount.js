511c5f952b8dd437dcec1e56895cd06599e7b2ac
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-15 05:43:51 -0700

    Update ButtonCount.js

--- ButtonCount.js
+++ ButtonCount.js
@@ -1,13 +1,13 @@
 // Set up counter
-        function Increment() {
-            let count = 0;
-            let btn = document.getElementById('counter');
+function Increment() {
+    let count = 0;
+    let btn = document.getElementById('counter');
             
-            btn.onclick = function () {
-                count++;
-                btn.innerHTML = 'Times Clicker: ' + count;
-            }
+    btn.onclick = function () {
+        count++;
+        btn.innerHTML = 'Times Clicker: ' + count;
         }
+    }
 class ButtonCount extends HTMLElement {
     constructor() {
         // Must call super() first in constructor
@@ -17,16 +17,15 @@ class ButtonCount extends HTMLElement {
         
         // Create button
         const button = document.createElement('button');
+        
         // Set button's attributes
         button.innerHTML = 'Times Clicker: 0'
         button.setAttribute('type', 'button'); // type='button'
         button.setAttribute('id', 'counter'); // id='counter
         button.setAttribute('onclick', 'Increment()') // onclick='Increment()'
         
-        
         document.querySelector('body').appendChild(button);
     }
-    
 }
 // Define ButtonCLicker class in custom elements registry so I may use it
 customElements.define('button-count', ButtonCount);
