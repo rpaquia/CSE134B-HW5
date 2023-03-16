73ac8cafd941fbe0d7fe9df8c22d2dffa037b611
rpaquia <97699019+rpaquia@users.noreply.github.com>
2023-03-15 17:30:04 -0700

    Update ButtonCount.js

--- ButtonCount.js
+++ ButtonCount.js
@@ -9,11 +9,12 @@ class ButtonCount extends HTMLElement {
         
         // Create button
         const button = document.createElement('button');
-        button.textContent = "Times Clicked";
+        button.textContent = `Times Clicked: ${count}`;
 
         button.addEventListener('click', () => {
             count++;
             console.log(`Times Clicked: ${count}`);
+            button.textContent = `Times Clicked: ${count}`;
             
         });
         shadow.appendChild(button);
